c5816aa70ff68d07007ab4800b8f07bde3c22516
Thiti Prasopsub <auethiti@gmail.com>
2025-06-02 14:13:35 +0700

    add be

---
+++ BE/Game-be/src/main/java/org/example/gamebe/GameBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.gamebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameBeApplication.class, args);
+    }
+
+}
